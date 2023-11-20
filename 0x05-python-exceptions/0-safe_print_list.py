8a91e6c05cd3af62413f3f8ec35558d0dbb57114
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-11-20 11:36:36 -0800

    Hot fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-# if my_list is None:
-#return (0)
+    if my_list is None:
+        return (0)
     m = 0
     for i in range(x):
         try:
             print("{}".format(my_list[a]), end="")
             m += 1
-	    print()
+            print()
         except IndexError:
             print()
             pass
