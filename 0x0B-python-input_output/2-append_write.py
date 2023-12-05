d71499cb447af8d51ac669d2e7c4f37b162f31e2
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-12-05 05:40:28 -0800

    Update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
- Append to a file module
+Append to a file module
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
     Append a string to a text file (UTF-8) and return
     the number of characters written.
+    
     :args:
         filename: add to this object
         text: string to append
+    
     :return: The number of characters written to the file.
-             Returns -1 on erroe.
+             Returns -1 on error.
     """
     with open(filename, mode="a", encoding="utf-8") as f:
-        if f.write(text):
-            return len(text)
+        return f.write(text)
