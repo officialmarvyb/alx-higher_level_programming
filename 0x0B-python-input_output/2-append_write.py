797840495a023bf3d402463e9271d3301d6cf1af
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-12-05 04:54:02 -0800

    Fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,13 +5,14 @@
 
 
 def write_file(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file.
-
-    Args:
-        filename (str): The name of the file to append to.
-        text (str): The string to append to the file.
-    Returns:
-        The number of characters appended.
+    """
+    Append a string to a text file (UTF-8) and return
+    the number of characters written.
+    :args:
+        filename: add to this object
+        text: string to append
+    :return: The number of characters written to the file.
+             Returns -1 on erroe.
     """
     with open(filename, mode="a", encoding="utf-8") as f:
         if f.write(text):
