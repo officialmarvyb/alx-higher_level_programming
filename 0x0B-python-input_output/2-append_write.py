fe6d6399c974372fdaabafe04a442ddae78670a3
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-12-05 05:42:04 -0800

    Update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,11 +8,11 @@ def append_write(filename="", text=""):
     """
     Append a string to a text file (UTF-8) and return
     the number of characters written.
-    
+
     :args:
         filename: add to this object
         text: string to append
-    
+
     :return: The number of characters written to the file.
              Returns -1 on error.
     """
