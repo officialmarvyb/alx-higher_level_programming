e56f439215f57d5966fd199dcaacaf90ac7b3ea7
officialmarvyb <marvellousadeyemi5@gmail.com>
2024-03-05 04:32:29 -0800

    debugging

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+curl -vH "X-School-User-Id: 98" "$1"
+
+# Check if curl command was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to send GET request."
+    exit 1
+fi
